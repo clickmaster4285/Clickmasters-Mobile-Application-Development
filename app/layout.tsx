147e56badb5a774b3bc58530867832d445b7abd6
clicmaster4285 <software.clickmasters@gmail.com>
2026-06-09 11:26:11 +0500

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" data-scroll-behavior="smooth">
+    <html lang="en" data-scroll-behavior="smooth" >
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
